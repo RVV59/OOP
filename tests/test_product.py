7681940399df59d50763dd73ad819dff1a66e9a4
RVlad <reshetov664@gmail.com>
2025-03-15 17:05:33 +0700

    update moduls, 15.3.2025 17:05:26

--- tests/test_product.py
+++ tests/test_product.py
@@ -25,3 +25,29 @@ def test_product_initialization_with_negative_quantity():
     with pytest.raises(ValueError) as exc_info:
         Product("Test Product", "This is a test product", 99.99, -10)
     assert str(exc_info.value) == "Quantity cannot be negative"
+
+
+def test_product():
+    # Создаем продукт через класс-метод
+    product_data = {
+        'name': 'Сыр',
+        'description': 'Голландский сыр',
+        'price': 200,
+        'quantity': 7
+    }
+    product = Product.new_product(product_data)
+
+    # Проверяем атрибуты продукта
+    assert product.name == "Сыр", "Ошибка в имени продукта."
+    assert product.price == 200, "Ошибка в цене продукта."
+    assert product.quantity == 7, "Ошибка в количестве продукта."
+
+    # Проверяем сеттер для цены
+    product.price = 250  # Корректная цена
+    assert product.price == 250, "Ошибка в установке новой цены."
+
+    product.price = -10  # Некорректная цена
+    assert product.price == 250, "Ошибка: цена изменилась на некорректное значение."
+
+    product.price = 200  # Новая цена ниже текущей
+    assert product.price == 250, "Ошибка: цена изменилась на меньшую."
